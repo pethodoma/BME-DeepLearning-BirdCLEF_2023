f61e1a52b8a708ca9c1b7e6739496dfa1aaa23f9
Csanad <csanad00058@gmail.com>
2023-10-20 23:54:49 +0200

    added the Labelencoder class

--- data.py
+++ data.py
@@ -55,5 +55,32 @@ def cleandata():
     print(f"Deleted {count} files\n")
 
 
+# Class for generating one-hot codes from the labels
+class LabelCoder:
+  # Extracting the categories from the header of a csv file (sample_submissions.csv in this case). Filepath, and the index of the first column in the header with a valid label are required arguments
+  def __init__(self, path_to_file, labels_start_in_header):
+    self.encoding_list = pd.read_csv(path_to_file, header=None).loc[0, labels_start_in_header:].tolist()
 
+  # Converting the labels to one-hot codes. This method considers the possibility for one file to have multiple labels
+  def encode(self, labels):
+    indices = []
+    # Gathering the indices of the labels in the original list
+    for label in labels:
+      if label in self.encoding_list:
+        indices.append(self.encoding_list.index(label))
+    # Creating an array with the size of the number of categories with all zero values
+    encoded_info = np.zeros(len(self.encoding_list))
+    # Replacing zeros with ones at the indices of the present categories
+    encoded_info[indices] = 1
+    return encoded_info
+
+  # Converting one-hot arrays to lists of categories
+  def decode(self, array):
+    # Gathering the indices of the ones in the
+    indices = np.where(array != 0)[0]
+    labels_list = []
+    for index in indices:
+        # Retrieving the appropriate label for the index from the original list of categories
+        labels_list.append(self.encoding_list[index])
+    return labels_list
 
